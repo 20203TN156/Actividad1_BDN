c73636e669b209ba86a313f15ea09b7e6ca5bfa8
CC7 <20203tn156@utez.edu.mx>
2022-01-10 11:55:41 -0600

    xd

---
+++ src/utez/gms/actividad1/Alumno.java
@@ -0,0 +1,9 @@
+package utez.gms.actividad1;
+
+public class Alumno {
+    private String matricula;
+    private String nombre;
+    private String fecha_nacimiento;
+
+
+}
---
+++ src/utez/gms/actividad1/Main.java
@@ -0,0 +1,8 @@
+package utez.gms.actividad1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
